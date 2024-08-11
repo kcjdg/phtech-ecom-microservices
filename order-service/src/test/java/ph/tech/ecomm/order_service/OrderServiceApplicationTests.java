7a607904c6543de06ee9fdbb9694b0d8b0b089b9
kcjdg <kencrisjohn@gmail.com>
2024-08-11 10:56:36 +0800

    rest assured testing

--- order-service/src/test/java/ph/tech/ecomm/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/ph/tech/ecomm/order_service/OrderServiceApplicationTests.java
@@ -1,15 +1,55 @@
 package ph.tech.ecomm.order_service;
 
+import io.restassured.RestAssured;
+import jakarta.websocket.server.ServerEndpoint;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+
+    @ServiceConnection
+    static MySQLContainer mysqlContainer = new MySQLContainer<>("mysql:8.3.0");
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mysqlContainer.start();
+
+    }
+
+    @Test
+    void shouldSubmitOrder() {
+    	var json = """
+				{
+				    "skuCode": "iphone_15",
+				    "price": 1000,
+				    "quantity": 101
+				}
+				""";
+		final var response = RestAssured.given()
+				.contentType("application/json")
+				.body(json)
+				.when()
+				.post("/api/order")
+				.then()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+		Assert.assertEquals("Order placed successfully", response);
 	}
 
 }
