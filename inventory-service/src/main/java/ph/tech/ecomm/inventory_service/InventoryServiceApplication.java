acaa4292b65a2ab027ffb06d275ca2a74d6e017c
kcjdg <kencrisjohn@gmail.com>
2024-08-11 11:32:49 +0800

    Initial commit for inventory service

---
+++ inventory-service/src/main/java/ph/tech/ecomm/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package ph.tech.ecomm.inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
