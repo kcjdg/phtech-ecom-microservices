ab779cba4635f1cea69bc14ee69b7912f0a74b09
kcjdg <kencrisjohn@gmail.com>
2024-08-11 15:22:32 +0800

    Use spring open feign client for synchronous call of microservice

--- order-service/src/main/java/ph/tech/ecomm/order_service/service/OrderService.java
+++ order-service/src/main/java/ph/tech/ecomm/order_service/service/OrderService.java
@@ -2,6 +2,7 @@ package ph.tech.ecomm.order_service.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ph.tech.ecomm.order_service.client.InventoryClient;
 import ph.tech.ecomm.order_service.dto.OrderRequest;
 import ph.tech.ecomm.order_service.model.Order;
 import ph.tech.ecomm.order_service.repository.OrderRepository;
@@ -13,13 +14,19 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product wih skuCode: "+orderRequest.skuCode()+" is out of stock");
+        }
     }
 }
