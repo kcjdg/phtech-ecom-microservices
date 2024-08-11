727f2c2e8cc2b98df0d9453063c633a34b6e05e0
kcjdg <kencrisjohn@gmail.com>
2024-08-11 15:23:01 +0800

    Use wiremock for testing the feign client

--- order-service/src/test/java/ph/tech/ecomm/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/ph/tech/ecomm/order_service/OrderServiceApplicationTests.java
@@ -8,10 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
+import ph.tech.ecomm.order_service.stubs.InventoryClientStub;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 
@@ -37,9 +40,12 @@ class OrderServiceApplicationTests {
 				{
 				    "skuCode": "iphone_15",
 				    "price": 1000,
-				    "quantity": 101
+				    "quantity": 1
 				}
 				""";
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+
 		final var response = RestAssured.given()
 				.contentType("application/json")
 				.body(json)
