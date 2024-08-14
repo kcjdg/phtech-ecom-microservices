615c2edbfa46ed58f0a605a84abd071fdf8603b8
kcjdg <kencrisjohn@gmail.com>
2024-08-14 22:03:39 +0800

    Kafka integration

--- order-service/src/main/java/ph/tech/ecomm/order_service/service/OrderService.java
+++ order-service/src/main/java/ph/tech/ecomm/order_service/service/OrderService.java
@@ -1,20 +1,26 @@
 package ph.tech.ecomm.order_service.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ph.tech.ecomm.order_service.client.InventoryClient;
 import ph.tech.ecomm.order_service.dto.OrderRequest;
+import ph.tech.ecomm.order_service.event.OrderPlacedEvent;
 import ph.tech.ecomm.order_service.model.Order;
 import ph.tech.ecomm.order_service.repository.OrderRepository;
 
 import java.util.UUID;
 
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -25,6 +31,13 @@ public class OrderService {
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            //send the message to kafka
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka order-placed", orderPlacedEvent);
+
         }else{
             throw new RuntimeException("Product wih skuCode: "+orderRequest.skuCode()+" is out of stock");
         }
