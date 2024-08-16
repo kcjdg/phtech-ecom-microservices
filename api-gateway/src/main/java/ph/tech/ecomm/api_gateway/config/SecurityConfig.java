790b5605771086bcbf1ec2bca1afe0959ad02874
kcjdg <kencrisjohn@gmail.com>
2024-08-16 21:47:35 +0800

    Monitoring and observability. Loki, tempo, prometheus

--- api-gateway/src/main/java/ph/tech/ecomm/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/ph/tech/ecomm/api_gateway/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
     private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
             "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
